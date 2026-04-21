79b381d4721f470dff3d00f42b3e543d02b774cd
aleksandrfetisov095-ui <aleksandrfetisov095@gmail.com>
2026-04-21 16:09:16 +0300

    Create __init__.py

---
+++ packets18lab/__init__.py
@@ -0,0 +1,10 @@
+from .longest import find_longest_words
+from .common import find_most_frequent_in_two, find_common_words
+from .palindrome import remove_palindromes
+
+__all__ = [
+    'find_longest_words',
+    'find_most_frequent_in_two',
+    'find_common_words',
+    'remove_palindromes'
+]
